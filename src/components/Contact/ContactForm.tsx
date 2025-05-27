5255cae1cc972a54825ea05591f29307fb9b9c1e
agusmena <agusmena60@mail.com>
2025-05-27 19:47:13 -0300

    agrego texto en la pagina contacto

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -1,11 +1,11 @@
 
 export default function ContactForm() {
     return (
-        <div className="my-4">
+        <div className="my-4 p-5">
             <h2 className="uppercase text-center text-xl text-blue-600">Déjanos tus datos</h2>
-            <div className="border-t w-3/4 mx-auto">
+            <div className="border-t mx-auto">
             </div>
-            <p></p>
+            <p className="my-5">Queremos ayudarte a encontrar la propiedad que estás buscando. Déjanos tus datos para que podamos contactarte y brindarte asesoramiento personalizado.</p>
         </div>
     )
 }
--- src/components/Contact/ContactoInfo.tsx
+++ src/components/Contact/ContactoInfo.tsx
@@ -1,12 +1,11 @@
 
 export default function ContactoInfo() {
     return (
-        <div className="my-4">
-            <div>
-                <h2 className="uppercase text-center  text-xl text-blue-600 ">Contáctanos</h2>
-                <div className="border-t w-3/4 mx-auto">
-                </div>
+        <div className="my-4 p-5">
+            <h2 className="uppercase text-center text-xl text-blue-600">contáctanos</h2>
+            <div className="border-t mx-auto">
             </div>
+            <p className="my-5">En BMZ Propiedades, hacemos de tu búsqueda una experiencia simple, clara y confiable.</p>
         </div>
     )
 }
