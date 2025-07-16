54174a59489b34bee275d1310139e630025bb16d
agusmena <agusmena60@mail.com>
2025-07-16 17:04:23 -0300

    agrego fondo a la pagina de prop encontradas

--- src/components/Propiedades/PropietyCard.tsx
+++ src/components/Propiedades/PropietyCard.tsx
@@ -8,7 +8,7 @@ export default function PropietyCard() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-2">
       {searchResults.map((p) => (
-        <div key={p._id} className="mb-6 transition-all duration-300 ease-in-out hover:shadow-lg hover:scale-[1.02] rounded-lg overflow-hidden bg-white">
+        <div key={p._id} className="mb-6 transition-all duration-300 ease-in-out hover:shadow-lg hover:scale-[1.02] rounded-lg  overflow-hidden bg-white">
           <div className="relative">
             <a
               href={`https://wa.me/5491123456789?text=${encodeURIComponent(
@@ -24,7 +24,7 @@ export default function PropietyCard() {
           <img
             src={p.images[0]}
             alt={p.location}
-            className="rounded-t-xl mx-auto w-full h-60 object-cover"
+            className=" mx-auto w-full h-60 object-cover"
           />
           <div className="px-2 pt-6 pb-2 text-sm uppercase text-zinc-500">
             <p>{p.description.length > 27 ? `${p.description.slice(0, 27)}...` : p.description}</p>
--- src/screens/PropertyFound.tsx
+++ src/screens/PropertyFound.tsx
@@ -3,18 +3,19 @@ import PropietyCard from "../components/Propiedades/PropietyCard";
 import { usePropertyStore } from "../store/propertyStore";
 
 export default function PropertyFound() {
-    const { searchResults } = usePropertyStore()
+    const { searchResults} = usePropertyStore()
     return (
-        <div className="bg-zinc-500/50">
+        <div className="relative bg-[url('../../src/assets/carrusel/terreno.jpeg')] bg-cover  min-h-[60vh] ">
+            <div className="bg-black/60  min-h-[60vh]">
+                <div className="max-w-5xl mx-auto py-5 px-4">
+                    <h2 className="text-white text-lg font-semibold mb-6">BUSCANDO: </h2>
+                    {searchResults.length > 0 ? <PropietyCard />
+                        : (<div className="flex justify-center">
+                            <p className="text-white" >No se encontraron coincidencias en la búsqueda </p> <TbMoodSadSquint size={25} color="white" />
 
-            <div className="max-w-5xl mx-auto py-5 px-4">
-                <h2 className="text-white text-lg font-semibold mb-6">BUSCANDO: </h2>
-                {searchResults.length > 0 ? <PropietyCard />
-                    : (<div className="flex justify-center">
-                        <p className="text-white" >No se encontraron coincidencias en la búsqueda </p> <TbMoodSadSquint size={25} color="white"/>
-
-                    </div> )}
-            </div></div>
+                        </div>)}
+                </div></div>
+        </div>
 
     )
 }
