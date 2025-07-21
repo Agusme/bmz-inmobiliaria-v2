8f1e21e4c28cdd650a64ebbab30389f95a09bc05
agusmena <agusmena60@mail.com>
2025-07-21 15:03:37 -0300

    cambio cel

--- src/components/BtnConsultarPrecio.tsx
+++ src/components/BtnConsultarPrecio.tsx
@@ -13,9 +13,9 @@ export default function BtnConsultarPrecio({ className = "" }: BtnConsultarPreci
     return (
        
                       <a
-                    href={`https://wa.me/5491123456789?text=${encodeURIComponent(
-                      `Hola, estoy interesada en la propiedad ubicada en ${propiedad?.location}`
-                    )}`}
+                  href={`https://wa.me/5493815633407?text=${encodeURIComponent(
+            `Hola, estoy interesada en la propiedad ubicada en ${propiedad?.location}`
+          )}`}
                     target="_blank"
                     rel="noopener noreferrer"
                   className={`btn bg-yellow-400 text-xs px-4 py-2 uppercase shadow-2xl rounded-3xl ${className}`}
