e841cfa9cd2345714112b832be1aa747c81b43a0
agusmena <agusmena60@mail.com>
2025-07-21 11:49:49 -0300

    arreglo color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: false,
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",  // Para que procese todos los archivos TS/JS en src
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Para que procese todos los archivos TS/JS en src
   ],
   theme: {
     extend: {},
   },
-plugins: [require("daisyui")],}
+  plugins: [require("daisyui")],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"], 
+  },
+};
